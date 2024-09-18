caaaae18fa5f9242a8a8861034857591d87661e2
Šmegfi <feedopeedo@gmail.com>
2024-09-18 09:18:29 +0200

    update
    updating

--- src/app/main/routes/api/user.py
+++ src/app/main/routes/api/user.py
@@ -1,24 +1,21 @@
 import uuid
-from flask import render_template, request
+from flask import request
 from app.main import bp
 from app.logger import set_logger
-from app.main.api.user import post_user
 from datetime import datetime, timedelta
-from app.main.repositories.modal import parse_form, parse_args
 import requests as req
 from config import Config
 
 logger = set_logger(__name__)
 
-@bp.route('/create-user')
-def users():
+@bp.route('/api/create-user')
+def create_user():
     email = request.args.get("email")
     mac = request.args.get("mac")
     password = str(uuid.uuid4())
 
     date =  datetime.now() + timedelta(days=30)
     expire = str(date).removesuffix(f".{date.microsecond}")
-
     data={
         "user-id": email,
         "email": email,
@@ -26,6 +23,15 @@ def users():
         "expiration": expire,
         "comment": mac
     }
+
+    ## POST request to Forti (create guest user) ##
     result = req.post(Config.API_URL, json=data, headers={"Authorization": f"Bearer {Config.API_TOKEN}"}, verify=False)
     logger.info(result.json())
+
+    if result.status_code == 200:
+        logger.info(result.json())
+        return data
+    else:
+        logger.warning(result.json())
+
     return result.json()
--- src/run.py
+++ src/run.py
@@ -1,6 +1,8 @@
 from app import create_app
 from config import Config
 
+app = create_app()
+
 if __name__ == '__main__':
     if str(Config.ENV) == "dev":
         debug = True
