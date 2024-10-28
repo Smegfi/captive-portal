2da52c793fd988e012a73a21e4a82950d62cb48d
Filip Šmehyl <fila.smehyl@gmail.com>
2024-10-28 23:32:18 +0100

    adding support for sqlite db

--- src/config.py
+++ src/config.py
@@ -6,20 +6,32 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 logger = set_logger(__name__)
 loaded = load_dotenv()
 
-if loaded:
-    logger.info(".env loaded")
-else:
-    logger.warning(".env not found")
-
-
 class Config:
     ENV = os.getenv("CAPTIVEP10_ENVIRONMENT")
     PORT = os.getenv("CAPTIVEP10_PORT")
     HOST = os.getenv("CAPTIVEP10_HOST")
-
     API_TOKEN = os.getenv("CAPTIVEP10_API_TOKEN")
     API_URL = os.getenv("CAPTIVEP10_API_URL")
     DB_PATH = os.getenv("CAPTIVEP10_DB_PATH")
 
+    # FLASK CONFIGURATION
+    if not ENV:
+        ENV = "dev"
+    if not PORT:
+        PORT = "5000"
+    if not HOST:
+        HOST = "0.0.0.0"
+    if not DB_PATH:
+        raise EnvironmentError("Systémová proměnná CAPTIVEP10_DB_PATH nebyla nalezena.")
+    if not API_URL:
+        raise EnvironmentError("Systémová proměnná CAPTIVEP10_API_URL nebyla nalezena.")
+    if not API_TOKEN:
+        raise EnvironmentError("Systémová proměnná CAPTIVEP10_API_TOKEN nebyla nalezena.")
+    if not os.path.exists(DB_PATH):
+        logger.error(f"Soubor {DB_PATH} nebyl nalezen.")
+        logger.info(f"Vytvářím soubor {DB_PATH}")
+        f = open(f"{DB_PATH}", "x")
+        f.close()
+    
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_PATH}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
