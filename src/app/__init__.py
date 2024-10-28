dbfe47517394db768167dfcf41c2c6bd58695d4f
Filip Šmehyl <fila.smehyl@gmail.com>
2024-10-28 23:32:18 +0100

    adding support for sqlite db

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -5,7 +5,7 @@ from app.extensions import db
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-
+    
     # Register blueprints here
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
--- src/app/extensions.py
+++ src/app/extensions.py
@@ -1,5 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
 
-connection_string = "postgresql+psycopg2://captiveUser:@10.41.171.10:5432/captive-portal"
-
 db = SQLAlchemy()
