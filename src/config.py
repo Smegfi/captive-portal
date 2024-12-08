1fdb7c338dfeb6effbe5bc7d2eb0f0310bf766e2
Filip Šmehyl <fila.smehyl@gmail.com>
2024-12-08 23:17:22 +0100

    feature/task-40-databaze

--- src/config.py
+++ src/config.py
@@ -1,10 +1,11 @@
 import os
 from dotenv import load_dotenv
-from app.logger import set_logger
+# from app.logger import set_logger
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-logger = set_logger(__name__)
+# logger = set_logger(__name__)
 loaded = load_dotenv()
+
 
 class Config:
     ENV = os.getenv("CAPTIVEP10_ENVIRONMENT")
@@ -13,6 +14,7 @@ class Config:
     API_TOKEN = os.getenv("CAPTIVEP10_API_TOKEN")
     API_URL = os.getenv("CAPTIVEP10_API_URL")
     DB_PATH = os.getenv("CAPTIVEP10_DB_PATH")
+    LOG_PATH = os.getenv("CAPTIVEP10_LOG_PATH")
 
     # FLASK CONFIGURATION
     if not ENV:
@@ -22,11 +24,14 @@ class Config:
     if not HOST:
         HOST = "0.0.0.0"
     if not DB_PATH:
-        raise EnvironmentError("Systémová proměnná CAPTIVEP10_DB_PATH nebyla nalezena.")
+        raise EnvironmentError(
+            "Systémová proměnná CAPTIVEP10_DB_PATH nebyla nalezena.")
     if not API_URL:
-        raise EnvironmentError("Systémová proměnná CAPTIVEP10_API_URL nebyla nalezena.")
+        raise EnvironmentError(
+            "Systémová proměnná CAPTIVEP10_API_URL nebyla nalezena.")
     if not API_TOKEN:
-        raise EnvironmentError("Systémová proměnná CAPTIVEP10_API_TOKEN nebyla nalezena.")
-    
+        raise EnvironmentError(
+            "Systémová proměnná CAPTIVEP10_API_TOKEN nebyla nalezena.")
+
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_PATH}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
