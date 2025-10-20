46e958fcd2624dda74f97cda56a2394652c6b928
Filip Šmehyl <fila.smehyl@gmail.com>
2025-10-20 23:08:17 +0200

    removed props

--- src/app/(guest)/page.tsx
+++ src/app/(guest)/page.tsx
@@ -2,11 +2,7 @@ import { AuthHeader } from "@/components/auth/auth-header";
 import { GuestForm } from "@/components/guest-portal/guest-form";
 import Image from "next/image";
 
-export default async function Page({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) {
-   const params = await searchParams;
-
-   console.log(params);
-
+export default async function Page() {
    return (
       <div className="grid min-h-svh lg:grid-cols-2">
          <div className="flex flex-col gap-4 p-6 md:p-10">
