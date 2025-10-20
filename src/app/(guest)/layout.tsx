9a37359b0d82789ae43a714dcf9fe7e9fba25af5
Filip Šmehyl <fila.smehyl@gmail.com>
2025-10-20 23:12:06 +0200

    import sorting

--- src/app/(guest)/layout.tsx
+++ src/app/(guest)/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from "next";
-import "@public/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import "@public/globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
    title: "Praha 10 - Captive Portal",
