1579c381bdb7686b3dd7c31547fba6d96bc4d545
Filip Šmehyl <fila.smehyl@gmail.com>
2025-10-20 23:07:37 +0200

    remove logging

--- src/lib/device-hook.ts
+++ src/lib/device-hook.ts
@@ -3,12 +3,6 @@ import { UAParser } from "ua-parser-js";
 const getDeviceInfo = () => {
    const { ua, browser, cpu, device, engine, os } = UAParser(navigator.userAgent);
 
-   console.log("browser", browser);
-   console.log("cpu", cpu);
-   console.log("device", device);
-   console.log("engine", engine);
-   console.log("os", os);
-
    return { ua, browser, cpu, device, engine, os };
 };
 
