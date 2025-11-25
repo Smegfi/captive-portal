429908b0b173b2a9a3ae5e452a2c3c532fc7000a
Filip Šmehyl <fila.smehyl@gmail.com>
2025-11-25 22:05:57 +0100

    TOS - fix height for modal

--- src/components/guest-portal/tos-disclaimer.tsx
+++ src/components/guest-portal/tos-disclaimer.tsx
@@ -13,7 +13,7 @@ export function TosDisclaimer() {
                <DialogHeader>
                   <DialogTitle>Podmínky použítí Wi-Fi</DialogTitle>
                </DialogHeader>
-               <ScrollArea className="h-[80vh] w-full">
+               <ScrollArea className="h-[80dvh] w-full">
                   <div className="flex flex-col gap-4 p-0 sm:p-2 sm:pr-4 bg-secondary">
                      <div className="overflow-hidden">
                         <img src="TOS/TOS-1.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" />
