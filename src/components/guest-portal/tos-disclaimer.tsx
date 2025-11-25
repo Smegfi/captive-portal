7041a6795a3645f7c33e323731eebda65137f28a
Filip Šmehyl <fila.smehyl@gmail.com>
2025-11-25 21:56:38 +0100

    TOS - image scale for mobile devices

--- src/components/guest-portal/tos-disclaimer.tsx
+++ src/components/guest-portal/tos-disclaimer.tsx
@@ -9,16 +9,24 @@ export function TosDisclaimer() {
             <DialogTrigger asChild>
                <span className="text-primary underline cursor-pointer">podmínkami použítí Wi-Fi</span>
             </DialogTrigger>
-            <DialogContent className="max-w-4xl px-0 sm:px-4">
+            <DialogContent className="max-w-4xl sm:max-w-3xl px-0 sm:px-4">
                <DialogHeader>
                   <DialogTitle>Podmínky použítí Wi-Fi</DialogTitle>
                </DialogHeader>
                <ScrollArea className="h-[80vh] w-full">
                   <div className="flex flex-col gap-4 p-0 sm:p-2 sm:pr-4 bg-secondary">
-                     <img src="TOS/TOS-1.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto" />
-                     <img src="TOS/TOS-2.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto" />
-                     <img src="TOS/TOS-3.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto" />
-                     <img src="TOS/TOS-4.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto" />
+                     <div className="overflow-hidden">
+                        <img src="TOS/TOS-1.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" />
+                     </div>
+                     <div className="overflow-hidden">
+                        <img src="TOS/TOS-2.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" />
+                     </div>
+                     <div className="overflow-hidden">
+                        <img src="TOS/TOS-3.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" />
+                     </div>
+                     <div className="overflow-hidden">
+                        <img src="TOS/TOS-4.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" />
+                     </div>
                   </div>
                </ScrollArea>
             </DialogContent>
