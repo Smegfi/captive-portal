80467cfd593965048c30fd2e84ac8bfaf5f1c751
Filip Šmehyl <fila.smehyl@gmail.com>
2025-12-09 22:26:24 +0100

    Downloading TOS document - REFACTORING

--- src/components/guest-portal/tos-disclaimer.tsx
+++ src/components/guest-portal/tos-disclaimer.tsx
@@ -1,5 +1,8 @@
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Download } from "lucide-react";
+import Image from "next/image";
+import { Button } from "../ui/button";
 
 export function TosDisclaimer() {
    return (
@@ -16,19 +19,25 @@ export function TosDisclaimer() {
                <ScrollArea className="h-[80dvh] w-full">
                   <div className="flex flex-col gap-4 p-0 sm:p-2 sm:pr-4 bg-secondary">
                      <div className="overflow-hidden">
-                        <img src="TOS/TOS-1.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" />
+                        <Image src="/TOS/TOS-1.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" width={1000} height={1000} />
                      </div>
                      <div className="overflow-hidden">
-                        <img src="TOS/TOS-2.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" />
+                        <Image src="/TOS/TOS-2.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" width={1000} height={1000} />
                      </div>
                      <div className="overflow-hidden">
-                        <img src="TOS/TOS-3.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" />
+                        <Image src="/TOS/TOS-3.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" width={1000} height={1000} />
                      </div>
                      <div className="overflow-hidden">
-                        <img src="TOS/TOS-4.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" />
+                        <Image src="/TOS/TOS-4.png" alt="Podmínky použítí Wi-Fi" className="w-full h-auto scale-110 sm:scale-100" width={1000} height={1000} />
                      </div>
                   </div>
                </ScrollArea>
+               <Button variant="outline" className="mx-2" asChild>
+                  <a href="TOS.pdf" download>
+                     <Download className="w-4 h-4" />
+                     Stáhnout
+                  </a>
+               </Button>
             </DialogContent>
          </Dialog>
       </p>
