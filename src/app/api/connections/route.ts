872712bdacbb84b3466d16a324de1f4145578ae5
Filip Šmehyl <fila.smehyl@gmail.com>
2026-01-07 22:23:59 +0100

    Pagination for connection API

--- src/app/api/connections/route.ts
+++ src/app/api/connections/route.ts
@@ -1,8 +1,13 @@
 import { getLatestConnectionsAction } from "@/server/actions/connection-actions";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-   const result = await getLatestConnectionsAction();
+export async function GET(request: Request) {
+   const { searchParams } = new URL(request.url);
+   const itemsPerPage = searchParams.get("itemsPerPage") || "10";
+   const page = searchParams.get("page") || "1";
+   const search = searchParams.get("search") || "";
+
+   const result = await getLatestConnectionsAction({ itemsPerPage: parseInt(itemsPerPage), page: parseInt(page), search: search });
 
    if (result.serverError !== undefined) {
       return NextResponse.json(result.serverError, { status: result.serverError.status });
