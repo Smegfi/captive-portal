3c2bd45ecca19f66acc34248f9f4b2612bf6f6f8
Filip Šmehyl <fila.smehyl@gmail.com>
2026-03-23 23:01:19 +0100

    slight adjustments

--- src/components/pages/loading.tsx
+++ src/components/pages/loading.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function ApplicationLoading() {
    return (
-      <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-background/95">
+      <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-background/80">
          <div className="animate-pulse">
             <Image src="/heraldicky-znak.png" alt="Načítání stránky" width={220} height={220} priority className="h-auto w-[220px]" />
          </div>
